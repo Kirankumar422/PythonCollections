62c1f41dc5b03b312439a34bdc5e9d0c65ff125c
kirankumar <ckkumar222@gmail.com>
2019-09-27 18:39:38 +0530

    Variable declaration changes...

--- src/QueriesSaver.py
+++ src/QueriesSaver.py
@@ -2,17 +2,15 @@ import csv
 from datetime import date
 
 date = date.today()
-
 queryFile = '/home/kirankumar/Shared/Common Files/used_queries.csv'
 readFile = open(queryFile, 'r')
 printHeader = True
 length = len(readFile.readlines())
 print ("Total lines in file is {}".format(length))
-if (length >= 1):
+if length >= 1:
     writeFile = open(queryFile, "a")
     writer = csv.writer(writeFile, delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
-    i = length + 1
-    while (i):
+    while True:
         query = raw_input("Please enter query: ")
         desc = raw_input("Please enter the description of the query: ")
         writer.writerow([date, query, desc])
