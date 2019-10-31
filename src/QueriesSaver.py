9ac2eb73f5f0f0ef3a95e2e7bd01bc2fe48bbb0a
kirankumar <ckkumar222@gmail.com>
2019-10-31 16:04:26 +0530

    Changed code to run in Python3.7

--- src/QueriesSaver.py
+++ src/QueriesSaver.py
@@ -6,16 +6,16 @@ queryFile = '/home/kirankumar/Shared/Common Files/used_queries.csv'
 readFile = open(queryFile, 'r')
 printHeader = True
 length = len(readFile.readlines())
-print ("Total lines in file is {}".format(length))
+print("Total lines in file is {}".format(length))
 if length >= 1:
     writeFile = open(queryFile, "a")
     writer = csv.writer(writeFile, delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
     while True:
-        query = raw_input("Please enter query: ")
-        desc = raw_input("Please enter the description of the query: ")
+        query = input("Please enter query: ")
+        desc = input("Please enter the description of the query: ")
         writer.writerow([date, query, desc])
         writeFile.close()
-        print (" Queries file is updated with new entries")
+        print(" Queries file is updated with new entries")
         break
 
 
